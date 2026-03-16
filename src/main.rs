a9ce55b2a3aee0c3be9dcac0116fdf732a8d6134
Chuck Office <chuckgao68686@gmail.com>
2026-03-16 13:37:24 +0800

    test: test cd

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use axum::{Router, routing::get};
+
 fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
-fn main() {
-    println!("Hello, CI!");
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello CD!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
 
 #[cfg(test)]
